1846a92272a9916474295ee5ab0c708c6cc35431
EdwinLolo <edwinfedora@gmail.com>
2025-07-31 15:28:36 +0700

    login,register, and admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    useLocation,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
 } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
@@ -15,38 +15,40 @@ import EventsPage from "./pages/EventsPage";
 import VendorPage from "./pages/VendorPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
+// Admin imports
 import AdminLoginPage from "./pages/admin/AdminLoginPage";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 
 function AppContent() {
-    const location = useLocation();
-    const hideNavFooter = location.pathname.startsWith("/admin/");
-    return (
-        <>
-            {!hideNavFooter && <Navbar />}
-            <Routes>
-                <Route path="/admin/login" element={<AdminLoginPage />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/rentals" element={<RentalsPage />} />
-                <Route path="/events" element={<EventsPage />} />
-                <Route path="/vendor" element={<VendorPage />} />
+  const location = useLocation();
+  const hideNavFooter = location.pathname.startsWith("/admin/");
+  return (
+    <>
+      {!hideNavFooter && <Navbar />}
+      <Routes>
+        <Route path="/admin/login" element={<AdminLoginPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/rentals" element={<RentalsPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/vendor" element={<VendorPage />} />
 
-                {/* ADMIN */}
-                <Route path="/admin" element={<h1>Admin Dashboard</h1>} />
+        {/* ADMIN */}
+        <Route path="/admin" element={<AdminDashboard />} />
 
-                {/* Catch-all route for 404 */}
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-            {!hideNavFooter && <Footer />}
-        </>
-    );
+        {/* Catch-all route for 404 */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      {!hideNavFooter && <Footer />}
+    </>
+  );
 }
 
 function App() {
-    return (
-        <Router>
-            <AppContent />
-        </Router>
-    );
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
 }
 
 export default App;
--- src/models/auth.jsx
+++ src/models/auth.jsx
@@ -6,3 +6,16 @@ export const login = async (email, password) => {
     body: JSON.stringify({ email, password }),
   });
 };
+
+export const signup = async (
+  email,
+  password,
+  first_name, // Send separately
+  last_name, // Send separately
+  role
+) => {
+  return apiCall("/signup", {
+    method: "POST",
+    body: JSON.stringify({ email, password, role, first_name, last_name }),
+  });
+};
